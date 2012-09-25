dd673b32abc6b0b9379fe66a2941e0cedd3eb852
Daniel Yarlett <daniel.yarlett@gmail.com>
2012-09-25 11:01:49 -0700

    Test commit.

--- mnist/mnist.go
+++ mnist/mnist.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"shoehorn"
+	"github.com/yarlett/shoehorn"
 	"time"
 )
 
@@ -13,8 +13,9 @@ func main() {
 	fmt.Printf("Took %v to create data set of %d objects.\n", time.Now().Sub(t1), len(sh.ObjectIDs()))
 
 	// Define parameters.
-	step_size, l2, alpha, numepochs := 0.01, 0.04, 0.0, 2500
+	step_size, l2, alpha, numepochs := 0.01, 0.1, 0.0, 2500
 
 	// Perform gradient-descent with L2 punishment initially.
+	sh.Rescale(1e-6)
 	sh.Learn(step_size, l2, alpha, numepochs, "tmp/locations")
 }
